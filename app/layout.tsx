7fc75197681acbbf3ec05a203baeccf798712ee8
Senin Adın <senin_email_adresin@example.com>
2024-10-01 13:46:03 -0800

    Initial commit for Sugar Bank project

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import {Inter ,IBM_Plex_Serif} from "next/font/google";
+import "./globals.css";
+
+
+const inter = Inter({ subsets: ["latin"], variable: '--font--inter'});
+const IbmPlexSerif = IBM_Plex_Serif({
+  subsets: ['latin'],
+  weight: ["400","700"],
+  variable: '--font-ibm-plex-serif'
+})
+
+export const metadata: Metadata = {
+  title: "SugarBank",
+  description: "Sugar Bank is a modern banking platform for everyone.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className = {`${inter.variable} ${IbmPlexSerif.variable}`}>
+
+        {children}
+        
+      </body>
+    </html>
+  );
+}
